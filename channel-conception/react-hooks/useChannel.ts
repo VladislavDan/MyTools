a0171db99dd8542c8ee1ca5fb1caca3cef636185
Vladislav_Danilov <Vladislav_Danilov@epam.com>
2022-05-16 14:50:19 +0400

    Fixed data base name to fix a problem with the same data base for two sites

--- channel-conception/react-hooks/useChannel.ts
+++ channel-conception/react-hooks/useChannel.ts
@@ -3,14 +3,19 @@ import {Subscription} from 'rxjs';
 
 import {Channel} from '../Channel';
 
-export const useChannel = <A, D>(channel: Channel<A, D>, next?: (value: D) => void, additionalErrorHandler?: (error: Error) => void) => {
+export const useChannel = <A, D>(
+    channel: Channel<A, D>,
+    next?: (value: D) => void,
+    additionalErrorHandler?: (error: Error) => void,
+    deepEqual = true
+) => {
 
     const value = useRef<{ subscription: Subscription | null }>({
         subscription: null
     });
 
     if (!value.current.subscription || value.current.subscription.closed) {
-        const subscription = channel.subscribe(next, additionalErrorHandler, true);
+        const subscription = channel.subscribe(next, additionalErrorHandler, deepEqual);
         value.current.subscription = subscription;
     }
 
